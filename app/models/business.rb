99e0e8af00f1b3d34c029ed617ba7fc7f9e104fe
Jen Homann <jen.homann@agapered.com>
2017-03-11 03:52:43 +0000

    fixes dependent destroy on business categories

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,5 @@
 class Business < ApplicationRecord
-  has_many :business_categories, inverse_of: :business
+  has_many :business_categories, inverse_of: :business, dependent: :destroy
   has_many :categories, through: :business_categories, inverse_of: :businesses
 
   validates :name, presence: true
--- app/models/business_category.rb
+++ app/models/business_category.rb
@@ -1,6 +1,6 @@
 class BusinessCategory < ApplicationRecord
-  belongs_to :business, inverse_of: :business_categories, dependent: :destroy
-  belongs_to :category, inverse_of: :business_categories, dependent: :destroy
+  belongs_to :business, inverse_of: :business_categories
+  belongs_to :category, inverse_of: :business_categories
 
   validates :business, :category, presence: true
 end
