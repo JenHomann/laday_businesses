95d69aa5d037de7bf1450e5b83edc6e269c8151d
Jen Homann <jen.homann@agapered.com>
2017-04-07 20:34:38 +0000

    adds decl, applies data attributes for select2 and datatable

--- app/assets/javascripts/plugins.js
+++ app/assets/javascripts/plugins.js
@@ -1,13 +1,28 @@
-$(document).on('turbolinks:load',function(){
-  $('select.select2').select2({placeholder: 'Select Categories'});
+Decl.select('select[data-uses-select2]', function(scope) {
+  scope.match(function(element) {
+    var placeholder = $(element).data('placeholder');
+    $(element).select2({placeholder: placeholder});
+  });
 
-  if($('[id^=DataTables_Table]').length == 0){
-    $('table#businesses').DataTable();
-  }
+  scope.unmatch(function(element) {
+    var select2 = $(element).data('select2');
+
+    if(select2) {
+      select2.destroy();
+    }
+  });
 });
 
-$(document).on("turbolinks:before-cache", function() {
-  if($('select.select2').length > 0){
-    $('select.select2').select2('destroy');
-  }
+Decl.select('table[data-uses-datatable]', function(scope) {
+  scope.match(function(element) {
+    $(element).DataTable();
+  });
+
+  scope.unmatch(function(element) {
+    var datatable = $(element).data('datatable');
+
+    if(datatable) {
+      datatable.destroy();
+    }
+  });
 });
